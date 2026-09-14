6f8cba2f97648bb320965a29157c4a3059cf9bb0
Aleksandr <133794807+mendelev-main@users.noreply.github.com>
2026-09-14 12:19:16 +0300

    v130.15 - advance shared application version

--- PrilavokPOS/Web/js/core/constants.js
+++ PrilavokPOS/Web/js/core/constants.js
@@ -1,14 +1,8 @@
-// Prilavok POS shared constants.
-// This module is intentionally side-effect free.
-// Runtime wiring into pos.html is performed incrementally and must be tested on a physical iPad.
-
 (function (global) {
   'use strict';
-
   const root = global.PrilavokCore = global.PrilavokCore || {};
-
   root.Constants = Object.freeze({
-    appVersion: '130.14',
+    appVersion: '130.15',
     sourceOfTruth: 'local-pos',
     syncMode: 'manual-only'
   });
